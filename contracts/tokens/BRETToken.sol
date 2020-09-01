9b90837d25b13ae59c91c82351098ae34c7b021f
CryptoDegen <degens.money@gmail.com>
2020-09-01 00:41:56 -0400

    update bret token

--- contracts/tokens/BRETToken.sol
+++ contracts/tokens/BRETToken.sol
@@ -1,19 +1,48 @@
 pragma solidity 0.5.16;
 
-import "./BurnableMintableERC20.sol";
+import "../externals/Address.sol";
+import "../externals/SafeMath.sol";
+import "../externals/SafeERC20.sol";
+import "../externals/IERC20.sol";
+import "../externals/ERC20.sol";
+import "../externals/ERC20Detailed.sol";
 
-/**
- * @title BRETToken
- * @dev Bretton Protocol token
- */
-contract BRETToken is BurnableMintableERC20 {
-    constructor ()
-        BurnableMintableERC20(
-            "Bretton Protocol Token",
-            "BRET",
-            18
-        )
-        public
-    {
-    }
+contract BRETToken is ERC20, ERC20Detailed {
+  using SafeERC20 for IERC20;
+  using Address for address;
+  using SafeMath for uint;
+
+  address public governance;
+  mapping (address => bool) public minters;
+
+  constructor ()
+      public
+      ERC20Detailed(
+          "Bretton Protocol",
+          "BRET",
+          18
+      )
+  {
+      governance = msg.sender;
+  }
+
+  function mint(address account, uint amount) public {
+      require(minters[msg.sender], "!minter");
+      _mint(account, amount);
+  }
+
+  function setGovernance(address _governance) public {
+      require(msg.sender == governance, "!governance");
+      governance = _governance;
+  }
+
+  function addMinter(address _minter) public {
+      require(msg.sender == governance, "!governance");
+      minters[_minter] = true;
+  }
+
+  function removeMinter(address _minter) public {
+      require(msg.sender == governance, "!governance");
+      minters[_minter] = false;
+  }
 }
